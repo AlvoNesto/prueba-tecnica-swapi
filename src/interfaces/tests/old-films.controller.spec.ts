984d1bfc47a0f5ae17020c2ff2fd2df0b559c757
Alvaro Narváez <anarvaezsa@ransa.net>
2024-10-25 10:21:26 -0500

    complete old-films test

--- src/interfaces/tests/old-films.controller.spec.ts
+++ src/interfaces/tests/old-films.controller.spec.ts
@@ -6,6 +6,24 @@ import { Film } from '../../domain/entities/films.entity';
 describe('OldFilmsController', () => {
   let oldFilmsController: OldFilmsController;
   let filmsService: FilmsService;
+  const mockFilm: Film = {
+    id: '1',
+    entidad: 'movie',
+    title: 'A New Hope',
+    episode_id: 4,
+    opening_crawl: 'It is a period of civil war...',
+    director: 'George Lucas',
+    producer: 'Gary Kurtz, Rick McCallum',
+    release_date: '1977-05-25',
+    characters: ['1', '2', '3'],
+    planets: ['Tatooine', 'Alderaan'],
+    starships: ['X-wing', 'Death Star'],
+    vehicles: ['Sand Crawler'],
+    species: ['Human', 'Droid'],
+    created: '1977-05-25',
+    edited: '1978-05-25',
+    url: 'https://swapi.dev/api/films/1/',
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -15,6 +33,7 @@ describe('OldFilmsController', () => {
           provide: FilmsService,
           useValue: {
             getAllSwapiFilms: jest.fn(),
+            getSwapiFilmById: jest.fn(),
           },
         },
       ],
@@ -27,33 +46,28 @@ describe('OldFilmsController', () => {
   describe('getAllSwapiFilms', () => {
     it('should return an array of films', async () => {
       
-      const mockFilms: Film[] = [
-        {
-          id: '1',
-          entidad: 'movie',
-          title: 'A New Hope',
-          episode_id: 4,
-          opening_crawl: 'It is a period of civil war...',
-          director: 'George Lucas',
-          producer: 'Gary Kurtz, Rick McCallum',
-          release_date: '1977-05-25',
-          characters: ['1', '2', '3'],
-          planets: ['Tatooine', 'Alderaan'],
-          starships: ['X-wing', 'Death Star'],
-          vehicles: ['Sand Crawler'],
-          species: ['Human', 'Droid'],
-          created: '1977-05-25',
-          edited: '1978-05-25',
-          url: 'https://swapi.dev/api/films/1/',
-        },
-      ];
+      const mockFilms: Film[] = [mockFilm];
 
       jest.spyOn(filmsService, 'getAllSwapiFilms').mockResolvedValue(mockFilms);
 
       const result = await oldFilmsController.getAllSwapiFilms();
-      
+
       expect(result).toEqual(mockFilms);
       expect(filmsService.getAllSwapiFilms).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('getSwapiFilmById', () => {
+    it('should return a single film by ID', async () => {
+      const filmId = '1';
+
+      jest.spyOn(filmsService, 'getSwapiFilmById').mockResolvedValue(mockFilm);
+
+      const result = await oldFilmsController.getSwapiFilmById(filmId);
+
+      expect(result).toEqual(mockFilm);
+      expect(filmsService.getSwapiFilmById).toHaveBeenCalledWith(filmId);
+      expect(filmsService.getSwapiFilmById).toHaveBeenCalledTimes(1);
     });
   });
 });
